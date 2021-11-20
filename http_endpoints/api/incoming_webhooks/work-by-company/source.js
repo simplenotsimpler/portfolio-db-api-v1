4cb0b6a7d491e4a2227f6c038d9c5aa783ff5004
SimpleNotSimpler <simplenotsimpler@outlook.com>
2021-11-20 16:14:18 -0500

    Fix(work-by-company): remove sort

--- http_endpoints/api/incoming_webhooks/work-by-company/source.js
+++ http_endpoints/api/incoming_webhooks/work-by-company/source.js
@@ -5,64 +5,119 @@ exports = async function(payload) {
 
   const pipeline = 
   [
-   {
-     '$unwind': {
-       'path': '$highlights', 
-       'preserveNullAndEmptyArrays': true
-     }
-   }, {
-     '$project': {
-       'company': '$company', 
-       'endDate': {
-         '$ifNull': [
-           '$endDate', {
-             '$dateToString': {
-               'format': '%Y-%m-%d', 
-               'date': '$$NOW'
-             }
-           }
-         ]
-       }, 
-       'startDate': '$startDate', 
-       'website': '$website', 
-       'location': '$location', 
-       'position': '$position', 
-       'highlights': '$highlights'
-     }
-   }, {
-     '$group': {
-       '_id': '$company', 
-       'combinedHighlights': {
-         '$push': '$highlights'
-       }, 
-       'positions': {
-         '$addToSet': {
-           'position': '$position', 
-           'endDate': '$endDate', 
-           'startDate': '$startDate',
-           'location': '$location'
-         }
-       }
-     }
-   }, {
-     '$project': {
-       '_id': 0, 
-       'company': '$_id', 
-       'combinedHighlights': '$combinedHighlights', 
-       'companyEndDate': {
-         '$max': '$positions.endDate'
-       }, 
-       'companyStartDate': {
-         '$min': '$positions.endDate'
-       }, 
-       'positions': '$positions'
-     }
-   }, {
-     '$sort': {
-       'positions.endDate': -1
-     }
-   }
- ];
+    {
+      '$unwind': {
+        'path': '$highlights', 
+        'preserveNullAndEmptyArrays': true
+      }
+    }, {
+      '$project': {
+        'company': '$company', 
+        'endDate': {
+          '$switch': {
+            'branches': [
+              {
+                'case': {
+                  '$eq': [
+                    {
+                      '$ifNull': [
+                        '$endDate', null
+                      ]
+                    }, null
+                  ]
+                }, 
+                'then': '$$NOW'
+              }, {
+                'case': {
+                  '$ne': [
+                    '$endDate', null
+                  ]
+                }, 
+                'then': {
+                  '$toDate': '$endDate'
+                }
+              }
+            ], 
+            'default': '$endDate'
+          }
+        }, 
+        'startDate': {
+          '$toDate': '$startDate'
+        }, 
+        'website': '$website', 
+        'location': '$location', 
+        'position': '$position', 
+        'highlights': '$highlights'
+      }
+    }, {
+      '$group': {
+        '_id': '$company', 
+        'combinedHighlights': {
+          '$push': '$highlights'
+        }, 
+        'positions': {
+          '$addToSet': {
+            'position': '$position', 
+            'endDate': '$endDate', 
+            'startDate': '$startDate', 
+            'location': '$location'
+          }
+        }
+      }
+    }, {
+      '$project': {
+        '_id': 0, 
+        'company': '$_id', 
+        'combinedHighlights': '$combinedHighlights', 
+        'companyEndDate': {
+          '$max': '$positions.endDate'
+        }, 
+        'companyStartDate': {
+          '$min': '$positions.endDate'
+        }, 
+        'positions': '$positions'
+      }
+    }, {
+      '$project': {
+        'company': '$company', 
+        'combinedHighlights': '$combinedHighlights', 
+        'companyEndDate': {
+          '$dateToString': {
+            'format': '%Y-%m-%d', 
+            'date': '$companyEndDate'
+          }
+        }, 
+        'companyStartDate': {
+          '$dateToString': {
+            'format': '%Y-%m-%d', 
+            'date': '$companyStartDate'
+          }
+        }, 
+        'positions': {
+          '$map': {
+            'input': '$positions', 
+            'as': 'positions', 
+            'in': {
+              'position': '$$positions.position', 
+              'endDate': {
+                '$dateToString': {
+                  'format': '%Y-%m-%d', 
+                  'date': '$$positions.endDate'
+                }
+              }, 
+              'startDate': {
+                '$dateToString': {
+                  'format': '%Y-%m-%d', 
+                  'date': '$$positions.startDate'
+                }
+              }, 
+              'location': '$$positions.location'
+            }
+          }
+        }
+      }
+    }
+  ];
 
  const work = await mycollection.aggregate(pipeline).toArray();
 
